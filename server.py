bb629075c2a74c03fd79538269ed407e441441dd
unknown <mfigurska@student.agh.edu.pl>
2024-05-24 14:19:00 +0200

    works

--- server.py
+++ server.py
@@ -44,7 +44,7 @@ async def create_transaction(transaction: Transaction) -> dict:
     return {"src_account": src_account, "des_account": des_account, "amount": amount}
 
 @app.get("/account/{account_id}", status_code=status.HTTP_200_OK)
-async def get_account(account_id: int) -> dict:
+async def get_account(account_id: int):
     '''Returns account details(from table konto) for given account_id'''
     try:
         return await handler.query_konto(account_id)
@@ -52,7 +52,7 @@ async def get_account(account_id: int) -> dict:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Account not found: {e}")
 
 @app.get("/transaction/{account_id}", status_code=status.HTTP_200_OK)
-async def get_transaction(account_id: int) -> dict:
+async def get_transaction(account_id: int):
     '''Returns transactions details(from table transakcja) for given account_id'''
     try:
         return await handler.query_transakcja(account_id)
