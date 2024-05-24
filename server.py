ff7ae5619a2ee5424b28c3f8afd6e3cfe52f8c3c
unknown <mfigurska@student.agh.edu.pl>
2024-05-24 13:21:33 +0200

    api doc

--- server.py
+++ server.py
@@ -9,9 +9,9 @@ branch_conns = setup_database()
 handler = Handler(branch_conns=branch_conns)
 login_table = {}
 
-# post konta
 @app.post("/new_account/", status_code=status.HTTP_201_CREATED)
 async def create_account(account: Account) -> dict:
+    '''Creates new account in distributed database and returns account details'''
     pesel = account.pesel
     imie = account.first_name
     nazwisko = account.last_name
@@ -25,10 +25,9 @@ async def create_account(account: Account) -> dict:
     
     return {"pesel": pesel, "imie": imie, "nazwisko": nazwisko, "saldo": saldo}
 
-
-# post transakcji
 @app.post("/new_transaction/", status_code=status.HTTP_201_CREATED)
 async def create_transaction(transaction: Transaction) -> dict:
+    '''Creates new transaction in distributed database and returns transaction details'''
     src_account = transaction.src_account
     des_account = transaction.des_account
     amount = transaction.amount
@@ -39,17 +38,17 @@ async def create_transaction(transaction: Transaction) -> dict:
 
     return {"src_account": src_account, "des_account": des_account, "amount": amount}
 
-# get konta
 @app.get("/account/{account_id}", status_code=status.HTTP_200_OK)
 async def get_account(account_id: int) -> dict:
+    '''Returns account details(from table konto) for given account_id'''
     try:
         return handler.query_konto(account_id) # it should be json
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Account not found: {e}")
 
-# get transakcji
 @app.get("/transaction/{account_id}", status_code=status.HTTP_200_OK)
 async def get_transaction(account_id: int) -> dict:
+    '''Returns transactions details(from table transakcja) for given account_id'''
     try:
         return handler.query_transakcja(account_id) # it should be json
     except Exception as e:
