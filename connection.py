ba6cf2bde394c6d053f39819cf8f86a1e00e8c18
IwoSzczepaniak <iszczepaniak@student.agh.edu.pl>
2024-06-04 15:31:43 +0200

    connection to dockerified ssms

--- connection.py
+++ connection.py
@@ -1,12 +1,57 @@
+import pyodbc
 import aioodbc
 
 DRIVER_NAME = 'ODBC Driver 17 for SQL Server'
-SERVER_NAME = 'sql_server'
+SERVER_NAME = 'localhost'
+PORT = 1433  # Default port for SQL Server
+SA_USER = 'sa'
+SA_PASSWORD = '<YourStrong@Passw0rd>'
 BRANCH_DB_NAMES = ['BANK1', 'BANK2', 'BANK3']
 
-async def create_connection(database: str) -> aioodbc.Connection:
+def create_connection_sync(database: str):
     '''
-    Function to create connection to a database
+    Function to create a synchronous connection to a database
+
+    Args:
+    database (str): name of the database
+
+    Returns:
+    pyodbc.Connection
+    '''
+    connection_string = f'''
+        DRIVER={{{DRIVER_NAME}}};
+        SERVER={SERVER_NAME},{PORT};
+        DATABASE={database};
+        UID={SA_USER};
+        PWD={SA_PASSWORD};
+    '''
+    conn = pyodbc.connect(connection_string)
+    return conn
+
+def create_database(db_name: str):
+    '''
+    Function to create a single database if it doesn't exist
+
+    Args:
+    db_name (str): name of the database
+    '''
+    conn = create_connection_sync('master')
+    conn.autocommit = True  # Set autocommit to True
+    cursor = conn.cursor()
+    cursor.execute(f"IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'{db_name}') CREATE DATABASE {db_name};")
+    cursor.close()
+    conn.close()
+
+def create_databases():
+    '''
+    Function to create databases if they don't exist
+    '''
+    for db_name in BRANCH_DB_NAMES:
+        create_database(db_name)
+
+async def create_connection_async(database: str) -> aioodbc.Connection:
+    '''
+    Function to create an asynchronous connection to a database
 
     Args:
     database (str): name of the database
@@ -16,9 +61,10 @@ async def create_connection(database: str) -> aioodbc.Connection:
     '''
     connection_string = f'''
         DRIVER={{{DRIVER_NAME}}};
-        SERVER={{{SERVER_NAME}}};
-        DATABASE={{{database}}};
-        Trust_Connection=yes;
+        SERVER={SERVER_NAME},{PORT};
+        DATABASE={database};
+        UID={SA_USER};
+        PWD={SA_PASSWORD};
     '''
     conn = await aioodbc.connect(dsn=connection_string)
     return conn
@@ -30,5 +76,6 @@ async def setup_database() -> list[aioodbc.Connection]:
     Returns:
     list[aioodbc.Connection]
     '''
-    branch_conns = [await create_connection(db) for db in BRANCH_DB_NAMES]
+    create_databases() 
+    branch_conns = [await create_connection_async(db) for db in BRANCH_DB_NAMES]
     return branch_conns
--- makefile
+++ makefile
@@ -1,7 +1,9 @@
 all:
-	docker build -t my-backend-image-name .
-	docker build -t my-frontend-image-name ./frontend
-
-# python -m uvicorn server:app
+	docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=<YourStrong@Passw0rd>" `   -p "1433:1433" --name "sql1" --hostname "sql1" -d "mcr.microsoft.com/mssql/server:2022-latest"
+	python -m uvicorn server:app
 # in the frontend folder
-# python -m http.server | http-server.exe
+	python /frontend -m http.server | http-server.exe
+
+# docker build -t my-backend-image-name .
+# docker build -t my-frontend-image-name ./frontend
+
